b664e62876df49f9260ce82046b8f1ef5f7899f3
Harish Durga <durgaharish5@gmail.com>
2020-04-25 23:49:22 +0530

    More google stun servers added

--- resources/js/components/ChatRootComponent.vue
+++ resources/js/components/ChatRootComponent.vue
@@ -103,7 +103,16 @@
                 const peer = new Peer({
                     initiator,
                     stream: this.myVideoSrc,
-                    trickle:true
+                    trickle:true,
+                    config: { iceServers: [
+                        { urls: 'stun:stun.l.google.com:19302' },
+                        { urls: 'stun:stun1.l.google.com:19302' },
+                        { urls: 'stun:stun2.l.google.com:19302' }, 
+                        { urls: 'stun:stun3.l.google.com:19302' },
+                        { urls: 'stun:stun4.l.google.com:19302' }, 
+                        { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
+                        ] 
+                    }
                 });
                 peer.on('signal',(data)=>{
                     console.log("Peer On Signal");
